15ceb5f40ee052d4278d70a129880a50c323fcd7
Ronny Mokaya <ronniemokaya30@gmail.com>
2024-02-05 22:22:39 +0300

    Complete on the sign up page

--- frontend/app/src/views/SignUp/SignUpForm.vue
+++ frontend/app/src/views/SignUp/SignUpForm.vue
@@ -1,19 +1,22 @@
 <template>
-  <form action="" class="w-full items-center flex flex-col space-y-4">
+  <form action="" class="w-full mx-auto  flex flex-col space-y-4">
     <!-- Inputs -->
-    <div class="flex flex-col items-center  space-y-2">
-        <input type="text" placeholder="First Name" class="p-1 bg-transparent text-slate-50 border rounded-sm border-slate-50">
-        <input type="text" placeholder="Last Name" class="p-1 bg-transparent text-slate-50 border rounded-sm border-slate-50">
-        <input type="email" placeholder="Email" class="p-1 bg-transparent text-slate-50 border rounded-sm border-slate-50">
-        <input type="password" placeholder="Password" class="p-1 bg-transparent text-slate-50 border rounded-sm border-slate-50">
-        <input type="password" placeholder="Confirm Password" class="p-1 bg-transparent text-slate-50 border rounded-sm border-slate-50">
+    <div class="flex flex-col items-center   space-y-3">
+        <input type="text" placeholder="First Name" class="p-2 w-full mx-auto bg-transparent text-slate-50 border rounded-sm border-slate-50">
+        <input type="text" placeholder="Last Name" class="p-1 w-full mx-auto bg-transparent text-slate-50 border rounded-sm border-slate-50">
+        <input type="email" placeholder="Email" class="p-1 w-full mx-auto bg-transparent text-slate-50 border rounded-sm border-slate-50">
+        <input type="password" placeholder="Password" class="p-1 w-full mx-auto bg-transparent text-slate-50 border rounded-sm border-slate-50">
+        <input type="password" placeholder="Confirm Password" class="p-1 w-full mx-auto bg-transparent text-slate-50 border rounded-sm border-slate-50">
     </div>
     
     <!-- Button -->
-    <div class="flex flex-col space-y-1">
-        <button type="submit" class="px-3 py-2 rounded-sm bg-yellow-500">Sign Up</button>
+    <div class="flex flex-col space-y-2">
+        <button type="submit" class="px-3 py-2 rounded-sm bg-yellow-500/75 hover:bg-yellow-500/90">Sign Up</button>
         <span>Already have an account? <router-link to="/" class="text-yellow-400">Log In</router-link></span>
     </div>
+
+    <!-- Sign Up with Google -->
+    <a href="#" class="py-3 border border-gradient-to-r border-purple-500 border-pink-500 hover:border-pink-950">Sign up with Google</a>
   </form>
 </template>
 
