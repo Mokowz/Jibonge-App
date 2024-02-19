fa66fbc713713edb829a152487a4d0c1e64d5e15
Ronny Mokaya <ronniemokaya30@gmail.com>
2024-02-19 22:19:15 +0300

    Set up authentication system

--- backend/accounts/forms.py
+++ backend/accounts/forms.py
@@ -2,4 +2,14 @@ from django.contrib.auth.forms import UserChangeForm, UserCreationForm
 
 from .models import CustomUser
 
-class 
+class CustomUserCreationForm(UserCreationForm):
+
+    class Meta:
+        model = CustomUser
+        fields = ["first_name", "last_name"]
+
+class CustomUserChangeForm(UserChangeForm):
+
+    class Meta:
+        model = CustomUser
+        fields = ["first_name", "last_name"]
--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -3,9 +3,11 @@ from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 class CustomUser(AbstractUser):
+    # Remove the username field
     username = None
-    first_name = models.CharField(max_length=30)
-    last_name = models.CharField(max_length=30)
+
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
