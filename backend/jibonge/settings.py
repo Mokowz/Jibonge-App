90249753e85a25472fe1621b1dc11818eeb08486
Ronny Mokaya <ronniemokaya30@gmail.com>
2024-05-24 21:26:03 +0300

    Remove redis

--- backend/jibonge/settings.py
+++ backend/jibonge/settings.py
@@ -62,18 +62,18 @@ MIDDLEWARE = [
 ]
 
 # Redis as a session backend
-SESSION_ENGINE = "django.contrib.sessions.backends.cache"
-SESSION_CACHE_ALIAS = "default"
+# SESSION_ENGINE = "django.contrib.sessions.backends.cache"
+# SESSION_CACHE_ALIAS = "default"
 
-CACHES = {
-    "default": {
-        "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": "redis://127.0.0.1:6379/1",
-        "OPTIONS": {
-            "CLIENT_CLASS": "django_redis.client.DefaultClient",
-        }
-    }
-}
+# CACHES = {
+#     "default": {
+#         "BACKEND": "django_redis.cache.RedisCache",
+#         "LOCATION": "redis://127.0.0.1:6379/1",
+#         "OPTIONS": {
+#             "CLIENT_CLASS": "django_redis.client.DefaultClient",
+#         }
+#     }
+# }
 
 ROOT_URLCONF = 'jibonge.urls'
 
